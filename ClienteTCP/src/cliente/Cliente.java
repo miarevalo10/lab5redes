784a4d608db843a5aeb3c790fdb97882df805155
dmarino <dmarino10@uniandes.edu.co>
2017-03-26 19:20:00 -0500

    interfaz conectada

--- ClienteTCP/src/cliente/Cliente.java
+++ ClienteTCP/src/cliente/Cliente.java
@@ -1,21 +1,134 @@
 package cliente;
 
+import java.awt.Desktop;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Cliente {
 
 	public final static int SOCKET_PORT = 8081;      
 	public final static String SERVER = "127.0.0.1";  
-//	public final static String 	FILE_TO_RECEIVED = "data/modelado.pdf";  
-//
-//	public final static int FILE_SIZE = 1024*50000; 
+
+	private Socket socket;
+
+	private String fromServer;
+	private String archivo;
+	private String estado;
+
+	private FileOutputStream fos;
+	private BufferedOutputStream bos;
+
+	private PrintWriter pw;
+	private BufferedReader br;
+
+	private int bytesRead;
+	private int current = 0;
+
+	private String[] opciones;
+
+	public Cliente(){
+
+		fromServer = "";
+		archivo = "";
+		estado = "desconectado";
+	}
+
+	public String darEstado() {
+		return estado;
+	}
+
+	public void conectar() throws Exception{
+
+		socket = new Socket(SERVER, SOCKET_PORT);
+		System.out.println("Connecting...");
+		pw = new PrintWriter(socket.getOutputStream(), true);
+		br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		fromServer = br.readLine();
+		System.out.println("El servidor dijo " + fromServer);
+
+		if(fromServer.equals("HELLO it's me"));
+		{
+			pw.println("HELLO");
+
+			estado="conectado";
+		}		
+
+		fromServer = br.readLine();
+		System.out.println("El servidor dijo " + fromServer);
+
+		String[] temporal = fromServer.split(": ");
+		opciones = temporal[1].split(", ");
+
+	}
+	public String[] darOpciones(){
+		return opciones;
+	}
+
+	public void escogerArchivo(String nombreArchivo){
+		archivo=nombreArchivo;
+	}
+	public void iniciarDescarga() throws Exception{
+		
+		System.out.println(archivo);
+		pw.println(archivo);
+		
+		// receive file
+		//Lee el tamaño del archivo enviado or el servidor y crea el array de bytes con un tamaño
+		//mayor para poder guardarlo
+		int filesize = Integer.parseInt(br.readLine());
+		byte [] mybytearray  = new byte [filesize + 1024*1024];
+		System.out.println("tamaño " + filesize);
+		InputStream is = socket.getInputStream();
+		String path = "data/" + archivo + ".pdf";
+		fos = new FileOutputStream(path);
+		bos = new BufferedOutputStream(fos);
+
+		//			byte[] bytes = new byte[ 2048*16];
+		int bytes =2048*16;
+
+		bytesRead = is.read(mybytearray, current, mybytearray.length);
+		current = bytesRead;
+
+		while (bytesRead>-1) {
+			//Lee los bytes enviados y los va guardando en mybytearray, con un offset de current y
+			//de a paquetes de tamaño bytes
+			bytesRead = is.read(mybytearray, current, bytes);
+			if(bytesRead >= 0) current += bytesRead;
+			System.out.println("Recibiendo " + current/1024 + "KB de " + filesize/1024 + "KB");
+		}
+
+		//Escribe el array de bytes en el path definido en fileoutputstream
+		bos.write(mybytearray, 0 , current);
+		bos.flush();
+		System.out.println("File " + path
+				+ " downloaded (" + current + " bytes read)");
+		
+		mostrarArchivo();
+	}
+
+	public void detenerDescarga(){
+
+	}
+
+	public void mostrarArchivo() throws Exception{
+		File file = new File("data/" + archivo + ".pdf");
+		Desktop.getDesktop().open(file);
+	}
+
+	public void desconectar()throws IOException{
+		if (fos != null) fos.close();
+		if (bos != null) bos.close();
+		if (socket != null) socket.close();
+	}
+
 
 	public static void main (String [] args ) throws IOException {
 		int bytesRead;
@@ -57,7 +170,7 @@ public class Cliente {
 			fos = new FileOutputStream(path);
 			bos = new BufferedOutputStream(fos);
 
-//			byte[] bytes = new byte[ 2048*16];
+			//			byte[] bytes = new byte[ 2048*16];
 			int bytes =2048*16;
 
 			bytesRead = is.read(mybytearray, current, mybytearray.length);
@@ -70,7 +183,7 @@ public class Cliente {
 				if(bytesRead >= 0) current += bytesRead;
 				System.out.println("Recibiendo " + current/1024 + "KB de " + filesize/1024 + "KB");
 			}
-			
+
 			//Escribe el array de bytes en el path definido en fileoutputstream
 			bos.write(mybytearray, 0 , current);
 			bos.flush();
@@ -83,5 +196,4 @@ public class Cliente {
 			if (sock != null) sock.close();
 		}
 	}
-
 }
--- ClienteTCP/src/interfaz/InterfazPrincipal.java
+++ ClienteTCP/src/interfaz/InterfazPrincipal.java
@@ -10,10 +10,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+
+import cliente.Cliente;
 
 public class InterfazPrincipal extends JFrame implements ActionListener{
 
@@ -21,44 +27,149 @@ public class InterfazPrincipal extends JFrame implements ActionListener{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+
+	private JPanel temporal2;
 	private JTextField conexion;
-	
-	
+	private JComboBox archivos;
+
+	private Cliente mundo;
+
+	private JButton iniciar;
+	private JButton detener;
+	private JButton btnConexion;
+
+
 	public InterfazPrincipal(){
 
-		
+		mundo = new Cliente();
+
 		setTitle("Conexión TCP");
 		getContentPane( ).setLayout( new BorderLayout( ) );
-		getContentPane().setBackground( Color.WHITE);
-		setSize( 800, 620 );
+		setSize( 650, 200 );
 		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 
-		
+
 		JPanel con=new JPanel();
 		con.setLayout(new GridBagLayout());
-		
+		con.setBackground( Color.WHITE);
+
 		GridBagConstraints gbc = new GridBagConstraints( 0, 0, 1, 1, 0, 0, GridBagConstraints.LINE_START, GridBagConstraints.NONE, new Insets( 5, 5, 5, 5 ), 0, 0 );
 		con.add(new JLabel("Conexion:"),gbc);
 
-		conexion= new JTextField();
+		conexion= new JTextField(Cliente.SERVER + ":" + Cliente.SOCKET_PORT );
+		conexion.setBackground(Color.WHITE);
+		conexion.setBorder(new LineBorder(Color.RED));
 		conexion.setEditable(false);
 
 		gbc = new GridBagConstraints( 1, 0, 5, 1, 20, 0, GridBagConstraints.LINE_START, GridBagConstraints.BOTH, new Insets( 5, 5, 5, 5 ), 0, 0 );
 		con.add(conexion,gbc);
-		
-		JButton btnCon= new JButton("conectar");
-		btnCon.setActionCommand("conectar");
-		btnCon.addActionListener(this);
-		
+
+		btnConexion= new JButton("Conectar");
+		btnConexion.setActionCommand("Conectar");
+		btnConexion.addActionListener(this);
+
 		gbc = new GridBagConstraints( 6, 0, 5, 1, 0, 0, GridBagConstraints.LINE_START, GridBagConstraints.BOTH, new Insets( 5, 5, 5, 5 ), 0, 0 );
-		con.add(btnCon,gbc);
-		
+		con.add(btnConexion,gbc);
+
 		add(con,BorderLayout.NORTH);
 
-		JPanel archivos=new JPanel();
-		archivos.setLayout(new GridLayout(1,2));
-		
-		add(archivos,BorderLayout.CENTER);
+		temporal2 = new JPanel();
+		temporal2.setLayout(new BorderLayout());
+		temporal2.setBorder(new TitledBorder(mundo.darEstado()));
+		temporal2.setBackground( Color.WHITE);
+
+		JPanel arch=new JPanel();
+		arch.setBackground( Color.WHITE);
+		arch.setLayout(new GridLayout(3,2));
+
+		arch.add(new JLabel(""));
+
+		archivos= new JComboBox();
+		archivos.addActionListener(this);
+
+		archivos.setEnabled(false);
+		archivos.setBackground(Color.WHITE);
+		arch.add(archivos);
+
+		for(int i =0;i<4;i++){
+			arch.add(new JLabel(""));
+		}
+
+		temporal2.add(arch,BorderLayout.CENTER);
+
+		JPanel botones = new JPanel();
+		GridLayout l = new GridLayout(1,4);
+		l.setHgap(20);
+		botones.setLayout(l);
+		botones.setBackground( Color.WHITE);
+
+		iniciar = new JButton("Iniciar");
+		iniciar.setActionCommand("Iniciar");
+		iniciar.addActionListener(this);
+
+		detener = new JButton("Detener");
+		detener.setActionCommand("Detener");
+		detener.addActionListener(this);
+
+		botones.add(new JLabel(""));
+		botones.add(iniciar);
+		botones.add(detener);
+		botones.add(new JLabel(""));
+
+		temporal2.add(botones, BorderLayout.SOUTH);
+
+
+		add(temporal2,BorderLayout.CENTER);
+	}
+
+	public void darOpciones(){
+
+	}
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		if(event.getActionCommand().equals("Conectar")){
+			if(mundo.darEstado().equals("desconectado")){
+				try{
+					mundo.conectar();
+					conexion.setBorder(new LineBorder(Color.GREEN));
+					temporal2.setBorder(new TitledBorder(mundo.darEstado()));
+					btnConexion.setText("Desconectar");
+
+					for(int i=0; i<mundo.darOpciones().length;i++){
+						archivos.addItem(mundo.darOpciones()[i]);
+					}
+					archivos.setEnabled(true);
+
+				}
+				catch(Exception e){
+					JOptionPane.showMessageDialog( null, "Se detecto un error \n" + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE );
+				}
+			}
+			else if(mundo.darEstado().equals("conectado")){
+				try{
+					mundo.desconectar();
+					conexion.setBorder(new LineBorder(Color.RED));
+					temporal2.setBorder(new TitledBorder(mundo.darEstado()));
+					btnConexion.setText("Conectar");;
+
+					archivos.setEnabled(false);
+				}
+				catch(Exception e){		
+				}
+			}	
+		}
+		else if(event.getActionCommand().equals("Iniciar")){
+			try{
+				mundo.escogerArchivo((String) archivos.getSelectedItem());
+				mundo.iniciarDescarga();
+			}
+			catch(Exception e){
+				JOptionPane.showMessageDialog( null, "Se detecto un error \n" + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE );
+			}
+		}
+		else if(event.getActionCommand().equals("Detener")){
+			mundo.detenerDescarga();
+		}	
 	}
 
 	public static void main(String[] args) {
@@ -67,11 +178,4 @@ public class InterfazPrincipal extends JFrame implements ActionListener{
 		interfaz.setVisible( true );
 
 	}
-
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
